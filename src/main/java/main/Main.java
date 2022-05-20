a86a421426182eac6b3152f9dc8f35b675e50ed7
Junior Egger <junior.egger@gmail.com>
2022-05-20 19:39:33 -0300

    Informações do caminho do arquivo

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,34 +7,9 @@ import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Main {
     public static void main(String[] args) {
-
-        String strPath;
-        strPath = showInputDialog("ENTER A FOLDER PATH: ");
-        File path = new File(strPath);
-
-        File[] folders = path.listFiles(File::isDirectory);
-        File[] files = path.listFiles(File::isFile);
-
-        StringBuilder responseFolders = new StringBuilder();
-        StringBuilder responseFiles = new StringBuilder();
-
-        assert folders != null;
-        for (File folder : folders) {
-            responseFolders.append(folder).append("\n");
-        }
-
-        assert files != null;
-        for (File file : files) {
-            responseFiles.append(file).append("\n");
-        }
-
-        showMessageDialog(
-                null,
-                "FOLDERS\n" + responseFolders + "\n" +
-                        "FILES\n" + responseFiles);
-
-        boolean success = new File(strPath + "\\subdir").mkdir();
-        showMessageDialog(null, "DIRECTORY CREATED SUCCESSFULLY: " + success);
-
+        File path = new File(showInputDialog("ENTER A FILE PATH"));
+        showMessageDialog(null, "getName: " + path.getName());
+        showMessageDialog(null, "getParent: " + path.getParent());
+        showMessageDialog(null, "getPath: " + path.getPath());
     }
 }
