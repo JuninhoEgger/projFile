c6cf8b99e00b25e8fdda55896728bd17b9548fe4
Junior Egger <junior.egger@gmail.com>
2022-05-20 18:48:10 -0300

    First file read

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,30 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
 public class Main {
     public static void main(String[] args) {
 
+        File file = new File("C:\\temp\\in.txt");
+        Scanner leia = null;
+
+        try {
+            leia = new Scanner(file);
+            StringBuilder resposta = new StringBuilder();
+            while (leia.hasNextLine()) {
+                resposta.append(leia.nextLine() + "\n");
+            }
+            showMessageDialog(null, resposta);
+        } catch (IOException e) {
+            showMessageDialog(null, "ERROR: " + e.getMessage());
+        } finally {
+            if (leia != null) {
+                leia.close();
+            }
+        }
     }
 }
