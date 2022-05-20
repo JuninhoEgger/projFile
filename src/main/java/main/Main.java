0328a32f7bab128cf494a8d7ad880926e1ca75fd
Junior Egger <junior.egger@gmail.com>
2022-05-20 19:16:26 -0300

    FileWriter and BufferedWriter

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import static javax.swing.JOptionPane.showMessageDialog;
@@ -9,18 +9,18 @@ import static javax.swing.JOptionPane.showMessageDialog;
 public class Main {
     public static void main(String[] args) {
 
-        String path = "C:\\temp\\in.txt";
+        String[] lines = new String[]{"Good morning", "Good afternoon", "Good night"};
+        String path = "C:\\temp\\out.txt";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
-            StringBuilder response = new StringBuilder();
-            while (line != null) {
-                response.append(line).append("\n");
-                line = br.readLine();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
             }
-            showMessageDialog(null, response);
+            showMessageDialog(null, "DEU TUDO CERTO!");
         } catch (IOException e) {
-            showMessageDialog(null, "ERROR: " + e.getMessage());
+            e.printStackTrace();
         }
+
     }
 }
